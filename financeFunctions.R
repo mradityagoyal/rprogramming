2cb840051b36e815d384e4475c40bb0e1ea71722
Aditya Goyal <aditya.g.goyal@oracle.com>
2016-02-24 11:03:25 -0500

    add rounding and remove restriction on transaction type

--- financeFunctions.R
+++ financeFunctions.R
@@ -1,7 +1,7 @@
-process_transactions <- function(data, ticker, actions = "BUY") {
+process_transactions <- function(data, ticker) {
   #only for selected ticker
   sub <-
-    data[data$Ticker == ticker & data$Action %in% actions ,]
+    data[data$Ticker == ticker ,]
   sub <-
     sub[order(sub$Date, decreasing = TRUE), ]
   avgPurchasePrice_cb_total <- vector()
@@ -9,9 +9,9 @@ process_transactions <- function(data, ticker, actions = "BUY") {
     avgPurchasePrice_cb_total <-
       rbind(avgPurchasePrice_cb_total,
             c(
-              "CostBasis"=sum(sub$Amount[sub$Date <= d])
+              "CostBasis"=round(sum(sub$Amount[sub$Date <= d]), digits = 2)
               ,
-              "TotalShares"=sum(sub$NumShares[sub$Date <= d])
+              "TotalShares"=round(sum(sub$NumShares[sub$Date <= d]), digits = 2)
             ))
   }
   avg<-avgPurchasePrice_cb_total[,"CostBasis"]/avgPurchasePrice_cb_total[,"TotalShares"]
@@ -27,9 +27,13 @@ avgOfSeq <- function(data, tickers) {
 }
 
 trans <- read.csv("401KCleaned.csv")
+# remove rows with amount less than one dollar. messes up data visualizations 
+trans <- trans[abs(trans$Amount) > 1, ]
 # set date column as actual date
 trans$Date <- as.Date(trans$Date, "%m/%d/%Y")
 trans <- trans[order(trans$Date, decreasing = TRUE), ]
+# round up price to two
+trans$Price<- round(trans$Price, digits = 2)
 exploded <- avgOfSeq(trans, unique(trans$Ticker))
 library(ggplot2)
 qplot(Date, CostBasis, data=exploded, color = Ticker, geom = "line")
