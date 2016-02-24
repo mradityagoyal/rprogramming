1eefc2579ce8d389553567c1a7df921a1b569fd3
Aditya Goyal <aditya.g.goyal@oracle.com>
2016-02-24 13:13:06 -0500

    add the findTicker method

--- financeFunctions.R
+++ financeFunctions.R
@@ -1,7 +1,7 @@
-process_transactions <- function(data, ticker) {
+process_transactions <- function(data, investment) {
   #only for selected ticker
   sub <-
-    data[data$Ticker == ticker ,]
+    data[data$Investment == investment ,]
   sub <-
     sub[order(sub$Date, decreasing = TRUE), ]
   avgPurchasePrice_cb_total <- vector()
@@ -9,31 +9,70 @@ process_transactions <- function(data, ticker) {
     avgPurchasePrice_cb_total <-
       rbind(avgPurchasePrice_cb_total,
             c(
-              "CostBasis"=round(sum(sub$Amount[sub$Date <= d]), digits = 2)
+              "CostBasis" = round(sum(sub$Amount[sub$Date <= d]), digits = 2)
               ,
-              "TotalShares"=round(sum(sub$NumShares[sub$Date <= d]), digits = 2)
+              "TotalShares" = round(sum(sub$Shares[sub$Date <= d]), digits = 2)
             ))
   }
-  avg<-avgPurchasePrice_cb_total[,"CostBasis"]/avgPurchasePrice_cb_total[,"TotalShares"]
+  avg <-
+    avgPurchasePrice_cb_total[, "CostBasis"] / avgPurchasePrice_cb_total[, "TotalShares"]
   cbind(sub, avgPurchasePrice_cb_total, "AvgPurchasePrice" = avg)
 }
 
-avgOfSeq <- function(data, tickers) {
+avgOfSeq <- function(data, investments) {
   result <- vector()
-  for (t in tickers) {
-    result <- rbind(result, process_transactions(data, t))
+  for (inv in investments) {
+    result <- rbind(result, process_transactions(data, inv))
   }
   result
 }
 
-trans <- read.csv("401KCleaned.csv")
-# remove rows with amount less than one dollar. messes up data visualizations 
+findTicker <- function(x) {
+  tickers <-
+    cbind(
+      investment =
+        c(
+          "VANG INST INDEX PLUS",
+          "VANG TOT INTL STK IP",
+          "VANG EXT MKT IDX ISP",
+          "FID GROWTH CO POOL",
+          "VANGUARD TARGET 2050",
+          "VANG TOT INTL STK IS",
+          "VANGUARD TARGET 2060"
+        ),
+      ticker = c(
+        "VIIIX",
+        "VTPSX" ,
+        "VEMPX",
+        "MUTF:FIDG",
+        "MUTF:VT2050",
+        "VTSNX",
+        "MUTF:VT2060"
+      )
+    )
+  tickers <- as.data.frame(tickers)
+  as.character(tickers$ticker[as.character(tickers$investment) == x])
+}
+
+trans <-
+  read.csv("history.csv",
+           colClasses = c("character", "factor", "factor", "numeric", "numeric"))
+#keep change in market value rows for future.
+changeInMkt <-
+  trans[trans$Transaction.Type == "Change In Market Value",]
+#remove change in market value rows from transactions
+trans <-
+  trans[!trans$Transaction.Type == "Change In Market Value",]
+# remove rows with amount less than one dollar. messes up data visualizations
 trans <- trans[abs(trans$Amount) > 1, ]
 # set date column as actual date
 trans$Date <- as.Date(trans$Date, "%m/%d/%Y")
 trans <- trans[order(trans$Date, decreasing = TRUE), ]
-# round up price to two
-trans$Price<- round(trans$Price, digits = 2)
-exploded <- avgOfSeq(trans, unique(trans$Ticker))
+trans$Price <- round(trans$Amount / trans$Shares, digits = 2)
+exploded <- avgOfSeq(trans, unique(trans$Investment))
 library(ggplot2)
-qplot(Date, CostBasis, data=exploded, color = Ticker, geom = "line")
+qplot(Date,
+      CostBasis,
+      data = exploded,
+      color = Investment,
+      geom = "line")
